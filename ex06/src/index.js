cfe6b6f5c50ec9017cf37fd7dcdb6d0f00fb1b02
KatarinaRS <katarina.roser@gmail.com>
2022-04-06 18:21:57 +0200

    Add new properties to object

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+//Only change code below this line
+var lion = {
+    name: "Simba",
+    legs: 4,
+   tails: 1
+};
+function myFunction(propName, propValue) {
+    lion [propName] = propValue;
+    return lion;
+}
+console.log(myFunction("roar", "roar-roar"));
+// Only change code above this line
+module.exports = myFunction;

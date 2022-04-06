717e2e21eea8051224b1368b8e33f39444b54c5c
KatarinaRS <katarina.roser@gmail.com>
2022-04-06 20:56:40 +0200

    .hasOwnProperty method

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -3,9 +3,11 @@ function myFunction(myObj, checkProp) {
     
      if (myObj.hasOwnProperty(checkProp)) {
             return myObj[checkProp];
-        } 
-         return "Not Found";
-        }
+           } else {
+            return "Not found";
+         }
+         return "Change me!";
+    }
         console.log( myFunction({title:"Titanic", song: "My heart Will Go On", genre: "drama"}, "title"));
         console.log( myFunction({title:"Titanic", song: "My heart Will Go On", genre: "drama"}, "song"));
         console.log( myFunction({title:"Titanic", song: "My heart Will Go On", genre: "drama"}, "genre"));
